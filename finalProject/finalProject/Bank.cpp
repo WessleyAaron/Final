dfcd37e6ba6243783109bdea869ab353b83676b9
WessleyAaron <37811241+WessleyAaron@users.noreply.github.com>
2018-05-10 12:23:25 -0500

    update

---
+++ finalProject/finalProject/Bank.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+bool yesno = true;
+int userChoice, userBankChoice;
+string username, password;
+string usernameAttempt, passwordAttempt;
+double userBalanceMoney, moneyDeposit, moneyWithdrawal;
+
+int logoff()
+{
+	cout << "Goodbye\n" << endl;
+
+	system("pause"); //ends the program
+	return 0;
+}
+
+int withdrawal()
+{
+	if (userBalanceMoney == 0.00) //when you don't have any money in your account
+	{
+		cout << "You don't have any money to take out\n" << endl;
+	}
+
+	else if (userBalanceMoney > 0.00) //while you have at least 1 penny in your account
+	{
+		cout << "How much money would you like to withdrawal? $";
+		cin >> moneyWithdrawal; //user enters how much money to take out of the account
+
+		cout << "\n";
+
+		if (moneyWithdrawal > userBalanceMoney) //when you try to take out more money than you have
+		{
+			cout << "You don't have that much money to take out\n" << endl;
+		}
+
+		else if (moneyWithdrawal < userBalanceMoney)
+		{
+			userBalanceMoney = userBalanceMoney - moneyWithdrawal; //math that takes money out of the account
+
+			cout << "Thank you for your withdrawal\n" << endl;
+		}
+	}
+
+	//goes back to the account function
+
+	return 0;
+}
+
+int deposit()
+{
+	cout << "How much money would you like to deposit? $";
+	cin >> moneyDeposit; //user enters how much money to deposit
+
+	cout << "\n";
+
+	userBalanceMoney = userBalanceMoney + moneyDeposit; //math that adds deposit to what is already in the account
+
+	cout << "Thank you for your deposit\n" << endl;
+
+	//goes back to the account function
+
+	return 0;
+}
+
+int account()
+{
+	cout << fixed << showpoint;
+	cout << setprecision(2); //makes it so each money display shows 2 decimal points
+	do{
+		cout << "Your current balance is $" << userBalanceMoney << "\n" << endl; //shows your current balance
+		cout << "What would you like to do? [1] Deposit, [2] Withdrawal, [3] Log Off: "; //user chooses what to do
+		cin >> userBankChoice;
+
+		cout << "\n";
+
+		if (userBankChoice == 1)
+		{
+			deposit(); //takes you to the deposit function
+		}
+
+		else if (userBankChoice == 2)
+		{
+			withdrawal(); //takes you to the withdrawal function
+		}
+
+
+	} while (userBankChoice != 3);
+
+	logoff(); //takes you to the log off function
+
+	return 0;
+}
+
+int signIn() //where you sign in
+{
+	cout << "Username: ";
+	cin >> usernameAttempt; //where you attempt to log in, username attempt
+
+	cout << "\n";
+
+	cout << "Password: ";
+	cin >> passwordAttempt; //password attempt
+
+	cout << "\n";
+
+	if (usernameAttempt == username && passwordAttempt == password) //when your username and password both work
+	{
+		account(); //takes you to the account function
+	}
+
+	else //when you entered something wrong
+	{
+		cout << "Something was wrong. Please try again\n" << endl;
+
+		signIn(); //replays the sign in function
+	}
+
+	return 0;
+}
+
+int accountCreate() //where you create your account
+{
+	cout << "Please create a username: ";
+	cin >> username; //saves your username
+
+	cout << "\n";
+
+	cout << "Please create a password: ";
+	cin >> password; //saves your password
+
+	cout << "\n";
+
+	cout << "Account created. Please Log in" << endl;
+
+	cout << "\n";
+
+	signIn(); //takes you to the sign in function
+
+	return 0;
+}
+
+int main() //initial starting point
+{
+	int userSignIn, userCreate;
+
+	cout << "Welcome to the bank" << endl;
+	cout << "Do you have an accout? [1] Yes or [2] No: ";
+	cin >> userChoice;
+
+	cout << "\n";
+
+	if (userChoice == 1) //if you've already created an account before
+	{
+		cout << "Would you like to sign in? [1] Yes or [2] No: ";
+		cin >> userSignIn;
+
+		cout << "\n";
+
+		if (userSignIn == 1) //takes you to the sign in function
+		{
+			signIn();
+		}
+
+		else if (userSignIn == 2) //replays the main function
+		{
+			main();
+		}
+	}
+
+	else if (userChoice == 2) //if this is your first time running this program
+	{
+		cout << "Would you like to create an account? [1] Yes or [2] No: ";
+		cin >> userCreate;
+
+		cout << "\n";
+
+		if (userCreate == 1) //takes you to the account create function
+		{
+			accountCreate();
+		}
+
+		else if (userCreate == 2) //replays the main function
+		{
+			main();
+		}
+	}
+}
--- finalProject/finalProject/main.cpp
+++ finalProject/finalProject/main.cpp
@@ -1,203 +1,23 @@
 #include <iostream>
-#include <string>
 #include <iomanip>
 
 using namespace std;
 
-bool yesno = true;
-int userChoice, depositChoice, itemDepositNumber, itemWithdrawalNumber, withdrawalChoice;
-string username, password;
-string usernameAttempt, passwordAttempt;
-string itemDeposit, itemWithdrawal;
-double userBalanceMoney, moneyDeposit, moneyWithdrawal;
-string userBalanceItems;
-int userBankChoice;
-
-int account();
-
-int logoff()
+int map()
 {
-	cout << "Goodbye" << endl;
+	int userChoice;
 
-	system("pause");
-	return 0;
-}
-
-int withdrawal()
-{
-	cout << "What would you like to withdrawal? [1] Money or [2] Items" << endl;
-	cin >> withdrawalChoice;
-
-	if (userBalanceMoney == 0.00 && withdrawalChoice == 1)
-	{
-		cout << "You don't have any money to take out" << endl;
-
-		account();
-	}
-
-	else if (withdrawalChoice == 1)
-	{
-		cout << "How much money would you like to withdrawal?" << endl;
-		cout << "$";
-		cin >> moneyWithdrawal;
-
-		userBalanceMoney = userBalanceMoney - moneyWithdrawal;
-
-		if (moneyWithdrawal > userBalanceMoney)
-		{
-			cout << "You don't have that much money to take out" << endl;
-
-			userBalanceMoney = userBalanceMoney + moneyWithdrawal;
-
-			account();
-		}
-	}
-
-	else if (withdrawalChoice == 2)
-	{
-		cout << "Which item would you like to withdrawal?" << endl;
-		cin >> itemWithdrawal;
-
-		cout << "How many would you like to withdrawal?" << endl;
-		cin >> itemWithdrawalNumber;
-	}
-
-	cout << "Thank you for your withdrawal" << endl;
-
-	account();
-
-	return 0;
-}
-
-int deposit()
-{
-	cout << "What would you like to deposit? [1] Money or [2] Items" << endl;
-	cin >> depositChoice;
-
-	if (depositChoice == 1)
-	{
-		cout << "How much money would you like to deposit?" << endl;
-		cout << "$";
-		cin >> moneyDeposit;
-
-		userBalanceMoney = userBalanceMoney + moneyDeposit;
-	}
-
-	else if (depositChoice == 2)
-	{
-		cout << "Which item would you like to deposit?" << endl;
-		cin >> itemDeposit;
-
-		cout << "How many would you like to deposit?" << endl;
-		cin >> itemDepositNumber;
-	}
-
-	cout << "Thank you for your deposit" << endl;
-
-	account();
-
-	return 0;
-}
-
-int account()
-{
-	cout << fixed << showpoint;
-	cout << setprecision(2);
-
-	cout << "Your current balance is $" << userBalanceMoney << " and " << userBalanceItems << " in your item storage" << endl;
-	cout << "What would you like to do? [1] Deposit, [2] Withdrawal, [3] Log Off" << endl;
-	cin >> userBankChoice;
-
-	if (userBankChoice == 1)
-	{
-		deposit();
-	}
-
-	else if (userBankChoice == 2)
-	{
-		withdrawal();
-	}
-
-	else if (userBankChoice == 3)
-	{
-		logoff();
-	}
-
-	return 0;
-}
-
-int signIn()
-{
-	cout << "Username: ";
-	cin >> usernameAttempt;
-	cout << "Password: ";
-	cin >> passwordAttempt;
-
-	if (usernameAttempt == username && passwordAttempt == password)
-	{
-		account();
-	}
-
-	else
-	{
-		cout << "Something was wrong. Please try again" << endl;
-		signIn();
-	}
-
-	return 0;
-}
-
-int accountCreate()
-{
-	cout << "Please create a username: ";
-	cin >> username;
-	cout << "Please create a password: ";
-	cin >> password;
-
-	cout << "Account created. Please Log in" << endl;
-
-	signIn();
-
-	return 0;
-}
-
-int main()
-{
-	int userSignIn, userCreate;
-
-	cout << "Welcome to the bank" << endl;
-	cout << "Do you have an accout? [1]Yes or [2] No" << endl;
+	cout << "Would you like to go to the bank? [1] Yes or [2] No" << endl;
 	cin >> userChoice;
 
 	if (userChoice == 1)
 	{
-		cout << "Would you like to sign in? [1] Yes or [2] No" << endl;
-		cin >> userSignIn;
-
-		if (userSignIn == 1)
-		{
-			signIn();
-		}
-
-		else if (userSignIn == 2)
-		{
-			main();
-		}
+		
 	}
 
 	else if (userChoice == 2)
 	{
-		cout << "Would you like to create an account? [1] Yes or [2] No" << endl;
-		cin >> userCreate;
-
-		if (userCreate == 1)
-		{
-			accountCreate();
-		}
-
-		else if (userCreate == 2)
-		{
-			main();
-		}
+		system("pause");
+		return 0;
 	}
 }
